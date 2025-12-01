0f9416c4661132a46b291b92f17f6f21678613b8
bob awesome <bobisawesome07@outlook.com>
2025-12-01 11:33:24 -0600

    initialize project structure with Gradle and Kotlin setup

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    kotlin("jvm") version "2.2.20"
+}
+
+group = "org.bobisawesome"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+}
+kotlin {
+    jvmToolchain(21)
+}
