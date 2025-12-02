feecf0159dd654d7f7b1bdea656c7e135b0d98da
bob awesome <bobisawesome07@outlook.com>
2025-12-02 07:44:16 -0600

    Add Day 2 functionality with a new class and update main to handle day selection

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,6 +4,7 @@ fun main() {
     println("what day is it?")
     when (val day = readln()) {
         "1" -> Day01().main()
+        "2" -> Day02().main()
         else -> println("Day $day not implemented yet")
     }
 }
