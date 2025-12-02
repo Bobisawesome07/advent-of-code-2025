ff75a4b824eb02ecb91289afb0ea66051cba66f2
bob awesome <bobisawesome07@outlook.com>
2025-12-02 08:50:28 -0600

    fixed things + parse day 2 input file

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -2,6 +2,10 @@ package org.bobisawesome
 
 class Day02 {
     fun main(inputFile: List<String>) {
+        val input = inputFile[0].split(",")
+        for(thing in input) {
+            println("hi $thing")
+        }
 
     }
 }
