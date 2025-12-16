adf854f2aac38d77461ded6288ca9b61630d7bd5
bob awesome <bobisawesome07@outlook.com>
2025-12-16 14:27:21 -0600

    refactor Day03 implementation and update workspace settings for Days 06 to 12

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -3,22 +3,8 @@ package org.bobisawesome
 class Day03 {
     fun main(inputFile: List<String>) {
         var result = 0
-        for (line in inputFile) {
-            val nums: MutableList<Int> = line.map { it.digitToInt() }.toMutableList()
-            val biggest = nums.maxOrNull()
-            val biggestIndex = nums.indexOf(nums.maxOrNull())
-            val numsSecond: MutableList<Int> = nums.toMutableList()
-            val secondBiggest = numsSecond.maxOrNull()
-            val secondBiggestIndex = numsSecond.indexOf(numsSecond.maxOrNull())
+        val nums = inputFile.forEach { it.map { it.digitToInt() }.toMutableList() }
 
-            println("from $nums $biggest and $secondBiggest are the 2 biggest numbers")
-
-            if (secondBiggestIndex >= biggestIndex) {
-                result += (biggest.toString() + secondBiggest.toString()).toInt()
-            } else {
-                result += (secondBiggest.toString() + biggest.toString()).toInt()
-            }
-        }
         println("output joltage is $result")
     }
 }
