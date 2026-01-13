04ecaad8719fd49c775e723e6b909e26d522bc80
bob awesome <bobisawesome07@outlook.com>
2026-01-13 14:38:02 -0600

    refactor Day03 main function and add getJoltage method

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -3,8 +3,17 @@ package org.bobisawesome
 class Day03 {
     fun main(inputFile: List<String>) {
         var result = 0
-        val nums = inputFile.forEach { it.map { it.digitToInt() }.toMutableList() }
+
+        for (line in inputFile) {
+            val nums = line.map { it.digitToInt() }
+            result += getJoltage(nums)
+        }
 
         println("output joltage is $result")
+    }
+
+    fun getJoltage (joltageList: List<Int>): Int {
+        var result = 0
+        return 0
     }
 }
